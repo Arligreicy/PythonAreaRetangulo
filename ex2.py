af49853a5d43e591734fd189f9d6c701d4c9394d
arligreicy <arligreicy.silva@fatec.sp.gov.br>
2024-09-29 21:54:41 -0300

    primeiro commit

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# Solicita a largura do retângulo ao usuário
+largura = float(input("Digite a largura do retângulo: "))
+
+# Solicita a altura do retângulo ao usuário
+altura = float(input("Digite a altura do retângulo: "))
+
+# Calcula a área do retângulo
+area = largura * altura
+
+# Imprime a área
+print(f"A área do retângulo é: {area}")
